ab65c5290d5ae93b1f25f8bd96610b41a9e7999d
Lucas Rosa <lucasrosaconceicao86@gmail.com>
2025-01-06 11:07:22 -0300

    Initial

---
+++ src/components/PostCard/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+export const PostCard = ({ cover, id, title, body }) => {
+    return (
+        <div className="post">
+            <img src={cover} alt={title}/>
+            <div key={id} className="post-content">
+            <h2>{title}</h2>
+            <p>{body}</p>
+            </div>
+        </div>
+    );
+} 
---
+++ src/templates/Home/index.js
@@ -0,0 +1,99 @@
+import './style.css';
+
+import { Component } from 'react';
+import { loadPosts } from '../../utils/load-posts';
+import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
+
+class Home extends Component {
+
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 2, 
+    searchValue: ""
+  };
+
+  async componentDidMount() {
+     await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+    const postsAndPhotos = await loadPosts();   
+    this.setState({
+       posts: postsAndPhotos.slice(page, postsPerPage), 
+       allPosts: postsAndPhotos
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page, 
+      postsPerPage,
+      allPosts, 
+      posts
+    } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({posts, page: nextPage});
+  }
+
+  handleChange = (evt) => {
+    const {value} = evt.target;
+    this.setState({searchValue: value});
+  }
+
+  render() {
+    const { posts, page, postsPerPage, allPosts, searchValue} = this.state;
+    const noMorePost = (page + postsPerPage) >= allPosts.length;
+
+    const filteredPosts = !!searchValue ? 
+                            allPosts.filter(post => {
+                              return post.title.toUpperCase().includes(searchValue.toUpperCase());
+                            }) : 
+                            posts;
+
+    return (
+
+      <section className="container">
+
+
+        <div className="search-container">
+          {!!searchValue && (
+            <h1>Busca: {searchValue}</h1>
+          )} 
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
+          />
+        </div>
+
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts}/>
+        )} 
+
+        {filteredPosts.length == 0 && (
+          <p>Não existem posts</p>
+        )} 
+
+        {!searchValue && (
+          <div className="button-container">
+            <Button 
+              text="Carrega"
+              onClick={this.loadMorePosts} // Não é o evento e sim um atributo sendo passado
+              disabled={noMorePost}
+            />
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
